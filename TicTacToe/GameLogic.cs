f2eec4dc8ef09938c47edf08eff48bb5dbbac020
Theis <t.troejborg.j@gmail.com>
2017-07-28 12:45:43 +0200

    Add project files.

---
+++ TicTacToe/GameLogic.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TicTacToe
+{
+    class GameLogic
+    {
+        public Player[,] GameBoard { get; private set; }
+        public List<Point> PlayHistory { get; private set; }
+        public Player CurrentPlayer { get; set; }
+        public Player PlayerOne { get; private set; }
+        public Player PlayerTwo { get; private set; }
+        public bool isFinished { get; set; }
+
+        public GameLogic()
+        {
+            GameBoard = new Player[3, 3];
+            PlayHistory = new List<Point>();
+
+            PlayerOne = new Player("PlayerOne");
+            PlayerTwo = new Player("PlayerTwo");
+            CurrentPlayer = PlayerOne;
+        }
+
+        public void placeToken(int x, int y)
+        {
+            GameBoard[x, y] = CurrentPlayer;
+            PlayHistory.Add(new Point(x, y));
+            if (anyWinner(x, y))
+            {
+                return;
+            }
+            // Next Players Turn
+            CurrentPlayer = CurrentPlayer == PlayerOne ? PlayerTwo : PlayerOne;
+        }
+
+        public void undoMove()
+        {
+            Point temp = PlayHistory[PlayHistory.Count];
+            GameBoard[(int)temp.X, (int)temp.Y] = null;
+            PlayHistory.Remove(temp);
+            CurrentPlayer = CurrentPlayer == PlayerOne ? PlayerTwo : PlayerOne;
+        }
+    
+        private bool anyWinner(int x, int y)
+        {
+            int coherent = 0;
+            int width = GameBoard.GetLength(0);
+            int height = GameBoard.GetLength(1);
+
+            // Vertical win check
+            for (int yy = 0; yy < width; yy++)
+            {
+                if (GameBoard[x, yy] != null && GameBoard[x, yy] == CurrentPlayer)
+                {
+                    coherent++;
+                }
+                else
+                {
+                    coherent = 0;
+                }
+                if (coherent == 3)
+                {
+                    isFinished = true;
+                    return true;
+                }
+            }
+            coherent = 0;
+            // Horizontal win check
+            for (int xx = 0; xx < height; xx++)
+            {
+                if(GameBoard[xx,y] != null && GameBoard[xx, y] == CurrentPlayer)
+                {
+                    coherent++;
+                }
+                else
+                {
+                    coherent = 0;
+                }
+                if (coherent == 3)
+                {
+                    isFinished = true;
+                    return true;
+                }
+            }
+            coherent = 0;
+            // Static 3x3 diagonal down-going win check 
+            for (int xx = 0; xx < height; xx++)
+            {
+                if (GameBoard[xx, xx] != null && GameBoard[xx, xx] == CurrentPlayer)
+                {
+                    coherent++;
+                }
+                else
+                {
+                    coherent = 0;
+                }
+                if (coherent == 3)
+                {
+                    isFinished = true;
+                    return true;
+                }
+            }
+            coherent = 0;
+            // Static 3x3 diagonal up-going win check
+            int yyy = width - 1;
+            for (int xx = 0; xx < height; xx++)
+            {
+                if (GameBoard[xx, yyy] != null && GameBoard[xx, yyy] == CurrentPlayer)
+                {
+                    coherent++;
+                    yyy--;
+                }
+                else
+                {
+                    coherent = 0;
+                }
+                if (coherent == 3)
+                {
+                    isFinished = true;
+                    return true;
+                }
+            }
+            coherent = 0;
+            return false;
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameLogic Game { get; set; }
+        public MainWindow()
+        {
+            Game = new GameLogic(); 
+            InitializeComponent();
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Label label = (Label) sender;
+            UniformGrid grid = (UniformGrid)label.Parent;
+
+            int cols = grid.Columns;
+            int rows = grid.Rows;
+            
+            int index = grid.Children.IndexOf(label);
+
+            int col = index % cols;
+            int row = index / cols;
+            
+            string token = Game.CurrentPlayer == Game.PlayerOne ? Game.PlayerOne.Name : Game.PlayerTwo.Name;
+            Game.placeToken(col, row);
+
+            label.Content = (token+"(" + col + "," + row + ")");
+
+            if (Game.isFinished)
+            {
+                MessageBox.Show(Game.CurrentPlayer.Name + " WON!");
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resetGameBoard();
+        }
+
+        private void updateGrid()
+        {
+            UniformGrid grid = this.GameGrid;
+            int cols = grid.Columns;
+            foreach (Label label in grid.Children)
+            {
+                int index = grid.Children.IndexOf(label);
+
+                int col = index % cols;
+                int row = index / cols;
+
+                if(Game.GameBoard[col,row] == null)
+                {
+                    label.Content = null;
+                }
+                else
+                {
+                    Player player = Game.GameBoard[col, row];
+                    label.Content = ("Reset: " + player.Name + "(" + col + "," + row + ")");
+                }
+            }
+        }
+        private void resetGameBoard()
+        {
+            Game = new GameLogic();
+            updateGrid();
+        }
+    }
+}
